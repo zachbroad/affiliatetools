07afaa20268d4041ba7ef62c0b1bbb4e3184232f
Zachary Broad <zachbroad@gmail.com>
2023-01-16 15:17:58 -0500

    ?

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from urllib.parse import quote, urlsplit, urlunsplit
 
 import pyperclip
 
-HEADING_FONT = ('System', 18)
+HEADING_FONT = ('TkDefaultFont', 18)
+FONT = ('TkDefaultFont', 12)
 
 root = tk.Tk()
 root.title("Affiliate Link Tool")
 root.geometry("500x300")
 
 TRACKING_LINK = 'https://prf.hn/click/camref:1100l8rZZ/destination:'
+DEFAULT_PADDING = {"pady": 2, "padx": 10}
 
 label = tk.Label(root, text="Affiliate Link", font=HEADING_FONT)
 label.pack(anchor='w')
 
 output_text = tk.StringVar()
-output_label = tk.Entry(root, textvariable=output_text, state='readonly', width=900)
+output_label = tk.Entry(root, textvariable=output_text, state='readonly', width=900, font=FONT)
 
 def remove_query_params(url:str)->str:
     parts = urlsplit(url)
     return urlunsplit((parts.scheme, parts.netloc, parts.path, '', ''))
+
+
 
 def on_input_update(*args):
     input_link = input_text.get()
@@ -27,22 +31,24 @@ def on_input_update(*args):
     if "chewy" in input_link:
         output = TRACKING_LINK + quote(input_link, safe='')
         output_text.set(output)
+        copytoclip()
     elif "amazon" in input_link:
         output = remove_query_params(input_link)
         output_text.set(output)
+        copytoclip()
     else:
         output_text.set('unknown input')
 
 
 input_text = tk.StringVar()
 input_text.trace("w", on_input_update)
-input_field = tk.Entry(root, textvariable=input_text, width=900)
-input_field.pack()
+input_field = tk.Entry(root, textvariable=input_text, width=900, font=FONT)
+input_field.pack(**DEFAULT_PADDING)
 
 label2 = tk.Label(root, text="Formatted Link", font=HEADING_FONT)
 label2.pack(anchor='w')
 
-output_label.pack()
+output_label.pack(**DEFAULT_PADDING)
 
 def copytoclip():
     pyperclip.copy(output_text.get())
@@ -50,6 +56,6 @@ def copytoclip():
     root.after(1500, lambda: copy_button.config(text="Copy to Clipboard"))
 
 copy_button = tk.Button(root, text="Copy to Clipboard", command=copytoclip)
-copy_button.pack()
+copy_button.pack(**DEFAULT_PADDING)
 
 root.mainloop()
